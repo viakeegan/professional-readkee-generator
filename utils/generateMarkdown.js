7c5dc9c6d8351344327644764708491b7a4ee449
unknown <viakeegan@gmail.com>
2022-01-09 20:08:52 -0500

    renderlicenseLink function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,14 +18,31 @@ function renderLicenseBadge(license) {
   }
 }
 
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license.length === 0) {
+    return ""
+  } else if (license == "Apache") {
+    return `https://opensource.org/licenses/Apache-2.0`
+  } else if (license == "Mozilla") {
+    return `https://opensource.org/licenses/MPL-2.0`
+  } else if (license == "MIT") {
+    return `https://opensource.org/licenses/MIT`
+  } else if (license == "GNU") {
+    return `https://www.gnu.org/licenses/gpl-3.0`
+  } else if (license == "Boost") {
+    return `https://www.boost.org/LICENSE_1_0.txt`
+  } else if (license == "ISC") {
+    return `https://opensource.org/licenses/ISC`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+ 
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
